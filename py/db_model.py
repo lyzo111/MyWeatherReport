1eff875ca024a852c1b3983a33ce46863f8ffc75
Lyzo <chrisus999@gmail.com>
2025-05-13 14:57:41 +0200

    add user authentication features; implement password hashing and update database models

--- py/db_model.py
+++ py/db_model.py
@@ -1,20 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 db = SQLAlchemy() # Create instance of SQLAlchemy
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True)
 
-    measurements = db.relationship('Measurement', backref='user', lazy=True)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Measurement(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, index=True)
     temperature = db.Column(db.Float)
     humidity = db.Column(db.Float)
     air_pressure = db.Column(db.Float)
+    location = db.Column(db.String(100))
 

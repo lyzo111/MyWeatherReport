330c82485c10bb539ef1a308c9d31ff9109e8c27
Lyzo <chrisus999@gmail.com>
2025-05-27 12:38:30 +0200

    removed template users

--- py/app.py
+++ py/app.py
@@ -36,34 +36,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-        # Create initial admin user if not exists
-        if not User.query.filter_by(username="admin").first():
-            user = User(
-                username="admin",
-                email="chrisus999@gmail.com"
-            )
-            user.set_password("secret123")  # hashed with werkzeug
-            db.session.add(user)
-            db.session.commit()
-
-            measurement1 = Measurement(
-                user_id=user.id,
-                timestamp=datetime.now(timezone.utc),
-                temperature=22.5,
-                humidity=55,
-                air_pressure=1013.2,
-                location="living room"
-            )
-            measurement2 = Measurement(
-                user_id=user.id,
-                timestamp=datetime.now(timezone.utc),
-                temperature=23.1,
-                humidity=53,
-                air_pressure=1012.8,
-                location="bedroom"
-            )
-
-            db.session.add_all([measurement1, measurement2])
-            db.session.commit()
-
     app.run(debug=True)
